87ec537f03e9f7c0e7b0ad4464b213e3938eb010
Nazar Shevtsov <nazarshevtsovboyron@gmail.com>
2026-04-09 20:30:15 +0200

    Herbivore and carnivore poprawione parametry w init

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
 class Animal:
     alive: list["Animal"] = []
 
-    def __init__(self,
-                 name: str,
-                 health: int = 100,
-                 hidden: bool = False) -> None:
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return (f"{{Name: {self.name},"
-                f" Health: {self.health},"
-                f" Hidden: {self.hidden}}}")
+        return (
+            f"{{Name: {self.name},"
+            f" Health: {self.health},"
+            f" Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
